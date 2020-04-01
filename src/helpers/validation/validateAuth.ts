bc2c05a4fae3bd11fa29c12355e25015d48b692f
ah <ah224cy@student.lnu.se>
2020-04-01 19:39:46 +0200

    working on route

--- src/helpers/validation/validateAuth.ts
+++ src/helpers/validation/validateAuth.ts
@@ -1,10 +1,10 @@
 import { validateEmail, validatePassword } from './checkUser'
 import { IAuthSigin } from '../../models/interface/auth'
 
+
 const validateAuth: (authCredentails: IAuthSigin) => Boolean = (authCredentails: IAuthSigin) => {
   const epostValidation: Boolean = validateEmail(authCredentails.email)
   const passwordValidation: Boolean = validatePassword(authCredentails.password)
-
 
   return epostValidation && passwordValidation ? true : false
 }
--- src/routes/signIn/index.ts
+++ src/routes/signIn/index.ts
@@ -1,13 +1,39 @@
 import { Router } from 'express'
 import { Request, Response } from 'express'
+import { Document } from 'mongoose'
+import User from '../../helpers/scheman/user'
+
 import validateAuth from '../../helpers/validation/validateAuth'
 import { IAuthSigin } from '../../models/interface/auth'
+import { IRoutes } from '../../models/interface/routes'
+import { IUser } from '../../models/interface/user'
 
 const side: Router = Router()
 
 
 side.route('/')
-  .post((req: Request, res: Response) => {
+  .post(async (req: Request, res: Response) => {
     const authCredentials: IAuthSigin = req.body
 
+    const checkInput: Boolean = validateAuth(authCredentials)
+
+    if (!checkInput) {
+
+      const obj: IRoutes = {
+        statusCode: 400,
+        message: 'no valid data in body'
+      }
+
+      res.status(400).send(obj)
+    }
+
+    try {
+      const user:any = await User.findOne({ email: authCredentials.email })
+
+      const passwordCheck = user.comparPassword(authCredentials.password)
+      
+
+    } catch (e) {
+      res.status(400).send('hello')
+    }
   })
